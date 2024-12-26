e6a9052bad3bbc5f84cff84be31709b07e8c4019
sharajaw <shailucareer@gmail.com>
2024-12-26 23:49:20 +0530

    Project renamed to VisualUiLibrary

--- ImageComparator/__init__.py
+++
@@ -1,7 +0,0 @@
-from .BrowserManager import BrowserManager
-from .CustomHtmlReportGenerator import CustomHtmlReportGenerator
-from .ExcelFileHandler import ExcelFileHandler
-from .ImageManager import ImageManager
-from .ImageComparator import ImageComparator
-
-__all__ = ["BrowserManager", "CustomHtmlReportGenerator", "ExcelFileHandler", "ImageManager", "ImageComparator"]
